62c4de6b004c8922fe9a679d91c8511d78436847
misaki <swimswiftcode@gmail.com>
2024-10-13 16:49:00 +0900

    basic認証部分を修正

--- server/src/openapi_server/apis/default_api.py
+++ server/src/openapi_server/apis/default_api.py
@@ -50,7 +50,7 @@ async def predicts_groups_post(
     ),
 ) -> object:
     """"""
-    return {"basic": token_basic.sub}
+    return {"basic": "success"}
     if not BaseDefaultApi.subclasses:
         raise HTTPException(status_code=500, detail="Not implemented")
     return await BaseDefaultApi.subclasses[0]().predicts_groups_post(talk_session_id, user_id)
